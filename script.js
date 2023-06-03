9472e20ef2546dd7f865c07a64565dbd4df7bf63
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-06-03 21:56:51 +0100

    initial commit of index.html and script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function getComputerChoice (){
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
 
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toLowerCase();
+        
+        let winningCombinations ={
+            rock: "scissors", paper: "rock", scissors: "paper"
+        };
+        if (playerSelection === computerSelection) {
+            return "It's a tie!";
+        }
+        if (winningCombinations[playerSelection] === computerSelection) {
+            
+        }
+    }
+
