ab5411f4da469b0c2a2c3f0d6bb006ca32ad9b35
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-06-11 00:58:20 +0100

    add playRound and game() function

--- script.js
+++ script.js
@@ -16,7 +16,32 @@ function getComputerChoice (){
             return "It's a tie!";
         }
         if (winningCombinations[playerSelection] === computerSelection) {
-            
+            return "You win!" + playerSelection + "beats" + computerSelection
         }
+        else 
+        return "You lose!" + computerSelection + "beats" + playerSelection
     }
 
+    function game() {
+        let playerScore = 0;
+        let computerScore = 0;
+
+        for (let i= 0; i < 5; i++) {
+            let playerSelection = prompt('Enter your choice (rock, paper, or scissors):');
+            let roundResult = playRound(playerSelection,computerSelection);
+            console.log(roundResult);
+
+            if (roundResult.includes('win')){
+                playerScore++;
+            } else if (roundResult.includes('lose')){
+                computerScore++;
+            }
+        }
+
+        if (playerScore > computerScore){
+            console.log("Congratulations! You're the boss.")
+        } else if (playerScore < computerScore) {
+            console.log("Sorry, you lose. Try again.")
+        }
+    }
+game();
